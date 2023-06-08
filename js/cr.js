7af540f6c6dba640425549918650ad983d1150ea
Mohd Suhail <mohdsuhail2232@gmail.com>
2023-06-08 23:13:16 +0530

    links nav and socialicon updated about us also

--- js/cr.js
+++ js/cr.js
@@ -1,22 +1,22 @@
-const sliderImages = document.querySelectorAll('.slider-image');
-const previewImages = document.querySelectorAll('.preview-image');
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
+const sliderImages = document.querySelectorAll(".slider-image");
+const previewImages = document.querySelectorAll(".preview-image");
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
 
 let currentIndex = 0;
 
 function showImage(index) {
-  const slider = document.querySelector('.slider-images');
+  const slider = document.querySelector(".slider-images");
   const slideWidth = sliderImages[0].clientWidth;
   slider.style.transform = `translateX(-${slideWidth * index}px)`;
 
   sliderImages.forEach((image, i) => {
     if (i === index) {
-      image.classList.add('active');
-      previewImages[i].classList.add('active');
+      image.classList.add("active");
+      previewImages[i].classList.add("active");
     } else {
-      image.classList.remove('active');
-      previewImages[i].classList.remove('active');
+      image.classList.remove("active");
+      previewImages[i].classList.remove("active");
     }
   });
 }
@@ -31,13 +31,22 @@ function showPrevImage() {
   showImage(currentIndex);
 }
 
-nextButton.addEventListener('click', showNextImage);
-prevButton.addEventListener('click', showPrevImage);
+nextButton.addEventListener("click", showNextImage);
+prevButton.addEventListener("click", showPrevImage);
 previewImages.forEach((image, i) => {
-  image.addEventListener('click', () => {
+  image.addEventListener("click", () => {
     showImage(i);
     currentIndex = i;
   });
 });
 
 showImage(currentIndex);
+
+function toggleOptions(selectElement) {
+  var container = selectElement.parentElement;
+  if (container.classList.contains("open")) {
+    container.classList.remove("open");
+  } else {
+    container.classList.add("open");
+  }
+}
