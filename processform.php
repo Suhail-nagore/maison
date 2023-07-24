aa149fa14d4835f01e47ee16283a0b05f67be4a5
Mohd Suhail <mohdsuhail2232@gmail.com>
2023-07-24 22:53:16 +0530

    database connection

--- processform.php
+++ processform.php
@@ -1,3 +1,4 @@
+
 <?php
 require 'PHPMailer.php';
 require 'SMTP.php';
@@ -15,9 +16,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Validate and sanitize the form data (perform necessary checks)
 
+    
+
     // Connect to the database
     $servername = "localhost"; // Replace with your MySQL server name
-    $username = "u339725174_maisonform1"; // Replace with your MySQL username
     $username = "u339725174_maisonform1"; // Replace with your MySQL username
     $password = "Maison@123"; // Replace with your MySQL password
     $dbname = "u339725174_maisonform"; // Replace with your MySQL database name
@@ -36,7 +38,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
         $stmt = $conn->prepare("INSERT INTO ContactForm (name,  email, contact_number, message) VALUES (:name,  :email, :contactNumber, :message)");
         $stmt->bindParam(':name', $name);
-        $stmt->bindParam(':name', $name);
         $stmt->bindParam(':email', $email);
         $stmt->bindParam(':contactNumber', $contactNumber);
         $stmt->bindParam(':message', $message);
@@ -51,43 +52,52 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $conn = null;
 
     // Send an email with the form data
-    $mail = new PHPMailer(true);
+   // Send an email with the form data
+// $mail = new PHPMailer(true);
 
 
 
 
 
-    try {
-        // Server settings
-        $mail->isSMTP();
-        $mail->Host = 'smtp.hostinger.com'; // Replace with your SMTP server address
-        $mail->SMTPAuth = true;
-        $mail->Username = 'info@maisoninfratech.in'; // Replace with your SMTP username
-        $mail->Password = 'Maison@123'; // Replace with your SMTP password
-        $mail->SMTPSecure = 'tls'; // Enable TLS encryption, 'ssl' also possible
-        $mail->Port = 587; // Update the port number to 587 for TLS
+// try {
+//     Server settings
+//     $mail->isSMTP();
+//     $mail->Host = 'smtp.hostinger.com'; // Replace with your SMTP server address
+//     $mail->SMTPAuth = true;
+//     $mail->Username = 'info@maisoninfratech.in'; // Replace with your SMTP username
+//     $mail->Password = 'Maison@123'; // Replace with your SMTP password
+//     $mail->SMTPSecure = 'tls'; // Enable TLS encryption, 'ssl' also possible
+//     $mail->Port =465 ;
 
-        // Recipients
-        $mail->setFrom($email, $name);
-        $mail->addAddress('info@maisoninfratech.in'); // Replace with the desired email address
+//     Recipients
+//     $mail->setFrom($email,$name);
+//     $mail->addAddress('info@maisoninfratech.in'); // Replace with the desired email address
 
-        // Email content
-        $mail->isHTML(false);
-        $mail->Subject = 'Form Submission';
-        $mail->Body = "Name: $name\n"
-            . "Email: $email\n"
-            . "Contact Number: $contactNumber\n"
-            . "Message: $message\n";
+//     Email content
+//     $mail->isHTML(false);
+//     $mail->Subject = 'Form Submission';
+//     $mail->Body = "Name: $name\n"
+        
+//         . "Email: $email\n"
+//         . "Contact Number: $contactNumber\n"
+        
+//         . "Message: $message\n";
 
-        $mail->send();
-        echo 'Mail Sent';
+//     $mail->send();
+//     print_r($mail);
+//     echo 'Mail SEnt';
 
-        // Email sent successfully
-        header("Location: success.html");
-        exit;
-    } catch (Exception $e) {
-        // Error sending email
-        echo "Error sending email: " . $mail->ErrorInfo;
-        exit;
-    }
+//     Email sent successfully
+//     header("Location: success.html");
+//     exit;
+// } catch (Exception $e) {
+//     Error sending email
+//     echo "Error sending email: " . $mail->ErrorInfo;
+//     exit;
+// }
+ header("Location: success.html");
+ exit;
+
 }
+?>
+
