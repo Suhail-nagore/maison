acd2471489596c8fbf6acc3da9648b564805241a
Mohd Suhail <mohdsuhail2232@gmail.com>
2023-08-09 15:00:57 +0530

    the .htaccess file is updated

--- js/script.js
+++ js/script.js
@@ -1,76 +1,73 @@
 (() => {
+  const openNavMenu = document.querySelector(".open-nav-menu");
+  const closeNavMenu = document.querySelector(".close-nav-menu");
 
+  const navMenu = document.querySelector(".nav-menu");
 
-    const openNavMenu = document.querySelector(".open-nav-menu");
-    const closeNavMenu = document.querySelector(".close-nav-menu");
+  const menuOverlay = document.querySelector(".menu-overlay");
 
-    const navMenu = document.querySelector(".nav-menu");
+  const mediaSize = 1070;
 
-    const menuOverlay = document.querySelector(".menu-overlay");
+  openNavMenu.addEventListener("click", togglenav);
+  closeNavMenu.addEventListener("click", togglenav);
 
-    const mediaSize = 1070;
+  // close the navmenu by clicking outside
+  menuOverlay.addEventListener("click", togglenav);
 
-    openNavMenu.addEventListener("click", togglenav);
-    closeNavMenu.addEventListener("click", togglenav);
+  function togglenav() {
+    navMenu.classList.toggle("open");
+    menuOverlay.classList.toggle("active");
+    document.body.classList.toggle(".hidden-scrolling");
+  }
 
-    // close the navmenu by clicking outside
-    menuOverlay.addEventListener("click", togglenav);
+  navMenu.addEventListener("click", (event) => {
+    if (
+      event.target.hasAttribute("data-toggle") &&
+      window.innerWidth <= mediaSize
+    ) {
+      const menuItemHasChildren = event.target.parentElement;
 
-    function togglenav() {
+      //if menuhaschildren is already expanded collaps it
 
-        navMenu.classList.toggle("open");
-        menuOverlay.classList.toggle("active");
-        document.body.classList.toggle(".hidden-scrolling");
+      if (menuItemHasChildren.classList.contains("active")) {
+        collapseSubMenu();
+      } else {
+        // //// ==============collapse existing expanded menuItemHasChildren
 
-    }
-
-    navMenu.addEventListener("click", (event) => {
-        if (event.target.hasAttribute("data-toggle") && window.innerWidth <= mediaSize) {
-            const menuItemHasChildren = event.target.parentElement;
-
-            //if menuhaschildren is already expanded collaps it
-
-            if (menuItemHasChildren.classList.contains("active")) {
-                collapseSubMenu();
-            } else {
-                // //// ==============collapse existing expanded menuItemHasChildren
-
-                if (navMenu.querySelector(".menu-item-has-children.active")) {
-                    collapseSubMenu();
-                }
-                //============== expand new menuItemHasChildren
-                menuItemHasChildren.classList.add("active");
-                const subMenu = menuItemHasChildren.querySelector(".sub-menu");
-                subMenu.style.maxHeight = subMenu.scrollHeight + "px";
-            }
-        }
-    });
-
-    function collapseSubMenu() {
-        navMenu.querySelector(".menu-item-has-children.active  .sub-menu")
-            .removeAttribute("style");
-        navMenu.querySelector(".menu-item-has-children.active")
-            .classList.remove("active");
-    }
-
-    function resizeFix() {
-        // if navMenu is open then clas it 
-        if (navMenu.classList.contains("open")) {
-            togglenav();
-        }
-        // if menu-menuItemHasChildren is expanded then collaps it 
         if (navMenu.querySelector(".menu-item-has-children.active")) {
-            collapseSubMenu();
+          collapseSubMenu();
         }
+        //============== expand new menuItemHasChildren
+        menuItemHasChildren.classList.add("active");
+        const subMenu = menuItemHasChildren.querySelector(".sub-menu");
+        subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+      }
     }
+  });
 
-    window.addEventListener("resize", function() {
-        if (this.innerWidth > mediaSize) {
-            resizeFix();
-        }
-    })
+  function collapseSubMenu() {
+    navMenu
+      .querySelector(".menu-item-has-children.active  .sub-menu")
+      .removeAttribute("style");
+    navMenu
+      .querySelector(".menu-item-has-children.active")
+      .classList.remove("active");
+  }
+
+  function resizeFix() {
+    // if navMenu is open then clas it
+    if (navMenu.classList.contains("open")) {
+      togglenav();
+    }
+    // if menu-menuItemHasChildren is expanded then collaps it
+    if (navMenu.querySelector(".menu-item-has-children.active")) {
+      collapseSubMenu();
+    }
+  }
+
+  window.addEventListener("resize", function () {
+    if (this.innerWidth > mediaSize) {
+      resizeFix();
+    }
+  });
 })();
-
-
-const whatsappButton = document.getElementById('whatsapp-button');
-whatsappButton.href = 'https://wa.me/+919690419681';
