b2acbdf8a4b9f83ddc19eb07467ac81b952d935d
Mohd Suhail <mohdsuhail2232@gmail.com>
2023-08-30 23:09:08 +0530

    captcha updated

--- processform.php
+++ processform.php
@@ -1,4 +1,3 @@
-
 <?php
 require 'PHPMailer.php';
 require 'SMTP.php';
@@ -9,107 +8,118 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $contactNumber = $_POST["contact-number"];
-    $message = $_POST["message"];
+$recaptcha_secret = "6LcTi-gnAAAAAB6cyzhKYXiEyjIixXTCmpkyj_mi"; // Replace with your secret key
+$recaptcha_response = $_POST['g-recaptcha-response'];
 
-    // Validate and sanitize the form data (perform necessary checks)
+$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$recaptcha_secret&response=$recaptcha_response");
+$responseKeys = json_decode($response, true);
 
-    
+if (intval($responseKeys["success"]) !== 1) {
+    echo "reCAPTCHA verification failed.";
+} else {
 
-    // Connect to the database
-    $servername = "localhost"; // Replace with your MySQL server name
-    $username = "u339725174_maisonform1"; // Replace with your MySQL username
-    $password = "Maison@123"; // Replace with your MySQL password
-    $dbname = "u339725174_maisonform"; // Replace with your MySQL database name
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $contactNumber = $_POST["contact-number"];
+        $message = $_POST["message"];
 
-    // Create a new PDO instance
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        // Handle database connection error
-        echo "Connection failed: " . $e->getMessage();
-        exit;
-    }
-
-    // Prepare and execute the database query
-    try {
-        $stmt = $conn->prepare("INSERT INTO ContactForm1 (name,  email, contact_number, message) VALUES (:name,  :email, :contactNumber, :message)");
-        $stmt->bindParam(':name', $name);
-        $stmt->bindParam(':email', $email);
-        $stmt->bindParam(':contactNumber', $contactNumber);
-        $stmt->bindParam(':message', $message);
-        $stmt->execute();
-    } catch (PDOException $e) {
-        // Handle database query error
-        echo "Error: " . $e->getMessage();
-        exit;
-    }
-        
-    // Close the database connection
-    $conn = null;
-       
-   
-    // Send an email with the form data 
-    $mail = new PHPMailer(true);
-    // print_r($mail);
+        // Validate and sanitize the form data (perform necessary checks)
 
 
 
+        // Connect to the database
+        $servername = "localhost"; // Replace with your MySQL server name
+        $username = "u339725174_maisonform1"; // Replace with your MySQL username
+        $password = "Maison@123"; // Replace with your MySQL password
+        $dbname = "u339725174_maisonform"; // Replace with your MySQL database name
 
-        
-    try {
-//  Server settings
-    // echo $mail;
-    // $mail->SMTPDebug = 2;                                       
-    $mail->isSMTP();
-    $mail->Host = 'smtp.hostinger.com'; // Replace with your SMTP server address
-    $mail->SMTPAuth = true;
-    $mail->Username = 'info@maisoninfratech.in'; // Replace with your SMTP username
-    $mail->Password = 'Maison@123'; // Replace with your SMTP password
-    $mail->SMTPSecure = 'ssl'; // Enable TLS encryption, 'ssl' also possible
-    $mail->Port = 465 ;
+        // Create a new PDO instance
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            // Handle database connection error
+            echo "Connection failed: " . $e->getMessage();
+            exit;
+        }
 
-//     Recipients
-    $mail->setFrom('info@maisoninfratech.in',$name);
-    $mail->addAddress('info@maisoninfratech.in'); // Replace with the desired email address
+        // Prepare and execute the database query
+        try {
+            $stmt = $conn->prepare("INSERT INTO ContactForm1 (name,  email, contact_number, message) VALUES (:name,  :email, :contactNumber, :message)");
+            $stmt->bindParam(':name', $name);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':contactNumber', $contactNumber);
+            $stmt->bindParam(':message', $message);
+            $stmt->execute();
+        } catch (PDOException $e) {
+            // Handle database query error
+            echo "Error: " . $e->getMessage();
+            exit;
+        }
 
-//     Email content
-    $mail->isHTML(true);
-    $mail->Subject = 'Form Submission';
-    $mail->Body = "Name: $name\n"
-        . "Email: $email\n"
-        . "Contact Number: $contactNumber\n"
-        . "Message: $message\n";
-    //     print_r($mail);
-    // if (!$mail->send()) {
-    // echo "Mailer Error: " . $mail->ErrorInfo;
-    // } else {
-    // echo "Message sent successfully!";
-    // }
-
-
-    $mail->send();
-    // print_r($mail);
-    // echo 'Mail SEnt';
-
-//     Email sent successfully
-    header("Location: success.html");
-    exit;
-} 
-catch (Exception $e) {
-    // Error sending email
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    exit;
-}
-    
+        // Close the database connection
+        $conn = null;
 
 
-//  header("Location: success.html");
+        // Send an email with the form data 
+        $mail = new PHPMailer(true);
+        // print_r($mail);
+
+
+
+
+
+        try {
+            //  Server settings
+            // echo $mail;
+            // $mail->SMTPDebug = 2;                                       
+            $mail->isSMTP();
+            $mail->Host = 'smtp.hostinger.com'; // Replace with your SMTP server address
+            $mail->SMTPAuth = true;
+            $mail->Username = 'info@maisoninfratech.in'; // Replace with your SMTP username
+            $mail->Password = 'Maison@123'; // Replace with your SMTP password
+            $mail->SMTPSecure = 'ssl'; // Enable TLS encryption, 'ssl' also possible
+            $mail->Port = 465;
+
+            //     Recipients
+            $mail->setFrom('info@maisoninfratech.in', $name);
+            $mail->addAddress('info@maisoninfratech.in'); // Replace with the desired email address
+
+            //     Email content
+            $mail->isHTML(true);
+            $mail->Subject = 'Form Submission';
+            $mail->Body = "Name: $name\n"
+                . "Email: $email\n"
+                . "Contact Number: $contactNumber\n"
+                . "Message: $message\n";
+            //     print_r($mail);
+            // if (!$mail->send()) {
+            // echo "Mailer Error: " . $mail->ErrorInfo;
+            // } else {
+            // echo "Message sent successfully!";
+            // }
+
+
+            $mail->send();
+            // print_r($mail);
+            // echo 'Mail SEnt';
+
+            //     Email sent successfully
+            header("Location: success.html");
+            exit;
+        } catch (Exception $e) {
+            // Error sending email
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            exit;
+        }
+
+
+
+        //  header("Location: success.html");
 //  exit;
-}
-?>
+    }
 
+}
+
+?>
